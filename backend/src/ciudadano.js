75c2f7db0bbc616b1bf36487e8a26ae773d4e898
Arkzado <jadiazosorio1@gmail.com>
2025-07-28 23:26:19 -0500

    endpoint editarCiudadano/:codigo

--- backend/src/ciudadano.js
+++ backend/src/ciudadano.js
@@ -46,4 +46,32 @@ ciudadano.post("/insertarCiudadano", async (req, res) => {
     }
 })
 
+ciudadano.put("/editarCiudadano/:codigo", async (req, res) => {
+    try {
+        const codigo = req.params.codigo;
+        const connection = await getConnection();
+        const datosCiudadano = {
+            nombre: req.body.nombre,
+            apellidos: req.body.apellidos,
+            apodo: req.body.apodo,
+            planeta_residencia: req.body.planeta_residencia,
+            foto: req.body.foto,
+            estado: req.body.estado
+        };
+        const consulta = "UPDATE ciudadano SET ? WHERE codigo = ?";
+        const [resultado] = await connection.query(consulta, [datosCiudadano, codigo]);
+
+        res.send({
+            estado: "ok",
+            data: resultado,
+        });
+    }
+    catch (err) {
+        res.send({
+            estado: "error",
+            data: err
+        })
+    }
+})
+
 export default ciudadano;
